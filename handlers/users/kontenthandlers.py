dcb5a0172d4f1c5cc417c8982f39f7e83fb0724b
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2023-01-07 14:13:31 +0500

    echo photo

---
+++ handlers/users/kontenthandlers.py
@@ -0,0 +1,11 @@
+from aiogram import types
+from aiogram.dispatcher import filters
+
+from loader import dp
+
+@dp.message_handler(content_types=types.ContentTypes.PHOTO)
+async def photo_handler(msg:types.Message):
+    chat_id = msg.chat.id
+    photo = msg.photo[0].file_id
+    print(photo)
+    await msg.answer_photo(photo)
