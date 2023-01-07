33176ef4b00936b197ceeb665fe32c15785d4b86
diyorbekmajidov <majidovdiyorbek741@gmail.com>
2023-01-07 14:20:01 +0500

    kanal link

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -4,6 +4,15 @@ from aiogram.dispatcher.filters.builtin import CommandStart
 from loader import dp
 
 
+@dp.message_handler(CommandStart(deep_link='APItelegram'))
+async def bot(message: types.Message):
+    args = message.get_args()
+    text = f'Salom,{message.from_user.full_name}!\n'
+    text += f'Sizni {args} tavsiya qildi'
+    await message.answer(text)
+
+
+@dp.message_handler(commands='start')
 @dp.message_handler(CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f'Salom,{message.from_user.full_name}!\n')
